fa99e7d73ab51b3f3078de9d82cd224571859693
aleksander <aleksander@normandy>
2026-09-07 20:20:19 +0200

    Read input stream
    And handle command line arguments.

--- mexec.c
+++ mexec.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 
 #define DEBUG_EXPR(expr) fprintf(stderr, "%s:%d:%s(): %s: 0x%llX\n", __FILE__, __LINE__, __func__, #expr, (unsigned long long)(expr))
 
 int main(int argc, char* argv[])
 {
-    printf("mexec\n");
-    //TODO: read input from stdin
-    //TODO: read input from file
+    FILE* infile = stdin;
+    if( 2 == argc )
+    {
+        infile = fopen(argv[1], "r");
+        if( !infile )
+        {
+            perror(NULL);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if( argc > 2 )
+    {
+        fprintf(stderr, "Too many arguments\nUsage: mexec <filename>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char linebuf[1025];
+    while( fgets(linebuf, sizeof(linebuf), infile) )
+    //TODO: error checking
+    {
+        printf("%s", linebuf);
+    }
+
+
     return 0;
 }
