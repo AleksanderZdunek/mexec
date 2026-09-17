1e0b523fd796b4813318681e6efb0d74d85825db
aleksander <aleksander@normandy>
2026-09-17 11:12:39 +0200

    Remove debug print macro

--- mexec.c
+++ mexec.c
@@ -9,8 +9,6 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 
-#define DEBUG_EXPR(expr) fprintf(stderr, "%s:%d:%s(): %s: 0x%llX\n", __FILE__, __LINE__, __func__, #expr, (unsigned long long)(expr))
-
 #define PIPE_FD_IDX_READ 0
 #define PIPE_FD_IDX_WRITE 1
 
